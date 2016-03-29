a6073b0e504f95d9e6843bd8b6398330b539ec5b
David Tomberlin <siyegen@gmail.com>
2016-03-29 00:18:32 -0700

    start of opengl window

---
+++ main.cpp
@@ -0,0 +1,51 @@
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (!window) {
+		std::cout << "Couldn't create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Couldn't init glew" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetKeyCallback(window, key_callback);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents(); // input
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // clear and draw color
+		glClear(GL_COLOR_BUFFER_BIT); // clear color buffer
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	// When a user presses the escape key, we set the WindowShouldClose property to true, 
+	// closing the application
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
