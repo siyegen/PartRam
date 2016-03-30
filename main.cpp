4e8fd46b99e78ba87e495ed0cc5cb9bc0d782ebc
David Tomberlin <siyegen@gmail.com>
2016-03-29 17:43:51 -0700

    some cleanup

--- main.cpp
+++ main.cpp
@@ -103,34 +103,23 @@ void runSample3(GLFWwindow* window) {
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
-	GLuint VBO, VAO, EBO;
+	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	//glGenBuffers(1, &EBO);
-	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
 	glBindVertexArray(VAO);
 
 	// Bind VBO here
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Position, adjust for color and vertex in same array
+	
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	// Color
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	//glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0); // Unbind VAO
 
 	glViewport(0, 0, WIDTH, HEIGHT);
 	glfwSetKeyCallback(window, key_callback);
-
-	// translate the scene in reverse of where we want to move
-
+	
 	const int numberOfCubes = sizeof(cubePositions) / sizeof(cubePositions[0]);
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents(); // input
